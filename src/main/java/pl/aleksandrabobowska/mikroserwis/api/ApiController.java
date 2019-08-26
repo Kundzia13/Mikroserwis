ac05e299a74112a22c5a7516ecf1b299980818c1
Kundzia13 <aleksandra.bobowska.93@gmail.com>
2019-08-26 20:52:05 +0200

    list all names

--- src/main/java/pl/aleksandrabobowska/mikroserwis/api/ApiController.java
+++ src/main/java/pl/aleksandrabobowska/mikroserwis/api/ApiController.java
@@ -3,13 +3,18 @@ package pl.aleksandrabobowska.mikroserwis.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.aleksandrabobowska.mikroserwis.api.dto.NameDto;
+import pl.aleksandrabobowska.mikroserwis.api.dto.ResponseDto;
 import pl.aleksandrabobowska.mikroserwis.database.entity.NameEntity;
 import pl.aleksandrabobowska.mikroserwis.database.repository.NameRepository;
 import pl.aleksandrabobowska.mikroserwis.util.Mappings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -36,5 +41,14 @@ public class ApiController {
         nameRepository.save(nameEntity);
         return ResponseEntity.ok()
                 .body(NameDto.fromEntity(nameEntity));
+    }
+    @GetMapping(Mappings.LIST_ALL_NAMES)
+    public ResponseEntity<ResponseDto> listAll() {
+        log.info("GET /{}", Mappings.LIST_ALL_NAMES);
+        List<NameDto> nameDtoList = new ArrayList<>();
+        nameRepository.findAll().forEach(e ->
+                nameDtoList.add(NameDto.fromEntity(e)));
+        return ResponseEntity.status(200)
+                .body(ResponseDto.builder().names(nameDtoList).build());
     }
 }
