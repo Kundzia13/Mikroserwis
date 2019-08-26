3862a0cd276b35579e9f18d993439ab0e6a40187
Kundzia13 <aleksandra.bobowska.93@gmail.com>
2019-08-26 20:05:19 +0200

    first commit

---
+++ src/main/java/pl/aleksandrabobowska/mikroserwis/MikroserwisApplication.java
@@ -0,0 +1,12 @@
+package pl.aleksandrabobowska.mikroserwis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikroserwisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikroserwisApplication.class);
+    }
+}
